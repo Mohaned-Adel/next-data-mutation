d6ad043747318d27f6575c358849eb9695c96106
Mohaned-Adel <mohaned.adel1996@gmail.com>
2025-04-15 11:31:46 +0200

    Configuring Dynamic Page Metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,15 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/posts";
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const postCount = posts.length;
+  return {
+    title: `All Posts (${postCount})`,
+    description: `All posts by all users (${postCount})`,
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
